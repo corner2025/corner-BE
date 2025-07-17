98e1fd15005d080b1ececf9ddae08c7e1fb70a2f
khakionblack <74r2k@naver.com>
2025-07-17 13:45:43 +0900

    관광지 컨트롤러, yml, cors 수정

--- src/main/java/com/corner/travel/config/CorsConfig.java
+++ src/main/java/com/corner/travel/config/CorsConfig.java
@@ -14,7 +14,13 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5000", "http://localhost:8080", "http://localhost:8081", "http://117.16.152.191:5000"));
+        config.setAllowedOrigins(List.of("http://localhost:5000",
+                                         "http://localhost:5001",
+                                         "http://localhost:5002",
+                                         "http://localhost:8080",
+                                         "http://localhost:8081",
+                                         "http://117.16.152.191:5000"));
+
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
